ba367302a169340d689dcf491013a7acd625469d
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-11-20 17:59:29 -0300

    fix: error message corrected in index.ts for SignUp form.

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,12 @@
 import * as z from "zod"
 
 export const SignupValidation = z.object({
-  name: z.string().min(2,{message: "Muito curto"}),
-  username: z.string().min(2,{message: "Nome de usuário muito curto"}).max(50,{message: "Nome de usuário muito longo"}),
-  email: z.string().email( {message: "E-mail inválido"}),
-  password: z.string().min(8, {message: "A senha deve ter pelo menos 8 caracteres."})
+  name: z.string().min(2, { message: "O nome deve ter pelo menos 2 caracteres." }),
+  username: z.string().min(2, { message: "O nome de usuário deve ter pelo menos 2 caracteres." })
+                     .max(50, { message: "O nome de usuário não pode ter mais de 50 caracteres." }),
+  email: z.string().email({ message: "Por favor, insira um endereço de e-mail válido." }),
+  password: z.string().min(8, { message: "A senha deve ter pelo menos 8 caracteres." })
+                      .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/, 
+                             { message: "A senha deve conter pelo menos 1 letra, 1 número e ter pelo menos 8 caracteres." })
 })
+
