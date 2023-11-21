3d305f16d2d5687e03c43dce67f99c9e8904e45a
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-11-21 16:52:42 -0300

    style: enhancing sign forms page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,16 +12,17 @@ const AuthLayout = () => {
         <Navigate to={"/"} />
       ) : (
         <>
-
-
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
 
-          <img src="/assets/images/side-img.svg" 
-          alt="Logomarca"
-          className="hidden xl:block h-screen w-1/2 object-cover  bg-no-repeat"
+
+          <img src="/assets/images/side-img.svg"
+            alt="Logomarca"
+            className="image-styling  object-cover  bg-no-repeat "
           />
+          <div className="image-styling bg-primary-500  absolute right-0 opacity-25" />
+
         </>
       )}
 
