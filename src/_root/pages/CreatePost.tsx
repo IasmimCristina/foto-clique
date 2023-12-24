c8b57c7e5b7c2c611f0d094714c122a7c691eede
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-12-24 14:20:25 -0300

    feat: updated and finnished create a post page.

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -7,7 +7,7 @@ const CreatePost = () => {
 
       <div className="common-container">
         <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-          <img src="assets/icons/add-post.svg" alt="Adicionar" width={36} height={36} />
+          <img src="/assets/icons/add-post.svg" alt="Adicionar" width={36} height={36} />
 
           <h2 className='h3-bold md:h2-bold text-left w-full'>Criar postagem</h2>
         </div>
