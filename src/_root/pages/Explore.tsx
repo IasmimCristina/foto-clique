fe2e526cdd8282b6213337a128d82ec6c1ec33e7
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-12-25 16:08:45 -0300

    style: changing explore style.

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -29,13 +29,16 @@ const {data: posts, fetchNextPage, hasNextPage} = useGetPosts();
     <div className="explore-container">
       <div className="explore-inner_container">
         <h2 className='h3-bold md:h2-bold w-full'>Procurar postagens</h2>
-        <div className="flex gap-1 pl-4 w-full rounded-lg bg-dark-4">
-          <img src="/assets/icons/search.svg" alt="Pesquisar" width={24} height={24} />
+        <div className="flex gap-1 pl-4 w-full rounded-lg ">
+         <div className="bg-dark-4  w-16 flex items-center rounded-md justify-center">
+          <img src="/assets/icons/search.svg" alt="Pesquisar" width={28} height={28} />
+
+         </div>
 
           <Input
             type='text'
             placeholder='Pesquisar'
-            className='explore-search'
+            className='explore-search '
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
@@ -46,7 +49,7 @@ const {data: posts, fetchNextPage, hasNextPage} = useGetPosts();
 
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h3 className='body-bold md:h3-bold'>Populares hoje</h3>
-        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer hover:bg-secondary-500 hover:rounded-full ease-in-out duration-300">
           <p className='small-medium md:base-medium text-light-2'>Todos</p>
 
           <img src="/assets/icons/filter.svg" alt="Filtro" width={20} height={20} />
@@ -55,6 +58,8 @@ const {data: posts, fetchNextPage, hasNextPage} = useGetPosts();
       <div className="flex flex-wrap gap-9 max-w-5xl">
         {shouldShowSearchResults ? (
           <SearchResults
+          isSearchFetching={isSearchFetching}
+          searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
           <p className='text-light-4 mt-10 text-center w-full'>Final das postagens</p>
--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,8 +1,20 @@
+import { Models } from 'appwrite'
 import React from 'react'
+import GridPostList from './GridPostList';
+import Loader from './Loader';
 
-const SearchResults = () => {
+type SearchResultsProps ={
+  isSearchFetching: boolean,
+   searchedPosts: Models.Document[];
+}
+const SearchResults = ({isSearchFetching, searchedPosts}:SearchResultsProps) => {
+
+if(isSearchFetching) return <Loader/>
+
+if(searchedPosts && searchedPosts.documents.length > 0) return (<GridPostList posts={searchedPosts.documents}/>)
+
   return (
-    <div>SearchResults</div>
+    <p className='text-light-4 mt-10 w-full text-center '>Nenhum resultado encontrado</p>
   )
 }
 
